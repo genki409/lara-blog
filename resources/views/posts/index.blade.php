31d6421d0a9449cf53332dce8bfef90d780b5227
genki <99254762+genki409@users.noreply.github.com>
2022-02-22 03:10:59 +0900

    contens→content

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app_original')
-@section('contens')
+@section('content')
   <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
