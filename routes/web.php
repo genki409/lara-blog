432064bb2aa08147ffaa8ef3accad7c3d125bd07
genki <99254762+genki409@users.noreply.github.com>
2022-02-26 16:39:06 +0900

    エラー処理

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,6 @@ Route::delete('/post/{id}', 'PostController@destroy')->name('posts.destroy');
 
 // Route::resource('/post', 'PostController');
 
-Route::get('/comment/create/{post_id}', 'CommentController@create')->name('comments.create');
+Route::get('/comments/create/{post_id}', 'CommentController@create')->name('comments.create');
 
-Route::post('/comments, 'CommentController@create')->name('comments.create');
+Route::post('/comments', 'CommentController@store')->name('comments.store');
