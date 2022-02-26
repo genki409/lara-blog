eacf4b187c6105cbc22eeabf40a240488db40f1a
genki <99254762+genki409@users.noreply.github.com>
2022-02-26 15:10:58 +0900

    コメント機能実装

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\comment
+use Illuminate\Support\Facades\Auth;
+
+
+use function PHPUnit\Framework\returnValue;
+
+
+class CommentController extends Controller
+{
+    public function create($post_id)
+    {
+        $post = Post::find($post_id);
+        return view('comments.create',compact('post'));
+    }
+
+    function store(Request $request)
+    {
+        $post - Post::find($request->post_id);
+        $comment = new Comment;
+        $comment -> body = $request->body;
+        $comment -> user_id = Auth::id();
+        $comment -> post_id = $request -> post_id;
+        $comment -> save;
+
+        return redirect()->route('posts.show', $post->id);
+    }
+}
